a9825f8e4ae6d4bf98f045467a1166a0cfa1fdc8
Stian Martinsen <stian.martinsen@try.no>
2026-01-16 11:28:17 +0100

    Upgrade packages + finish carousel demo

--- src/app/carousel/page.tsx
+++ src/app/carousel/page.tsx
@@ -18,32 +18,45 @@ export default function CarouselPage() {
 
 function Carousel({ children }: { children: React.ReactNode }) {
   return (
-    <div
-      className={classNames(
-        // Padding
-        "py-10",
+    <div className="py-10">
+      <div
+        className={classNames(
+          // Container/grid setup
+          "*:aspect-5/4 *:w-3/4 *:min-w-0 *:shrink-0",
 
-        // Aspect ratio
-        "[&>*]:aspect-[5/4] [&>*]:min-w-0",
+          // Mandatory scroll snap
+          "snap-mandatory snap-x *:snap-start",
 
-        // Mandatory scroll snap
-        "snap-mandatory snap-x [&>*]:snap-start",
+          // Images
+          "*:object-cover",
 
-        // Images
-        "[&>*]:object-cover",
+          // Grid setup
+          "overflow-auto flex gap-4",
 
-        // Grid setup
-        "overflow-auto grid grid-flow-col auto-cols-[80vw] gap-4",
+          // Scroll behavior
+          "scroll-smooth",
 
-        // Scroll behavior
-        "scroll-smooth",
+          // Scroll buttons
+          // - Give the carousel an anchor name
+          "[anchor-name:--carousel]",
+          // - Position the buttons
+          "[&::scroll-button(*)]:absolute [&::scroll-button(*)]:[position-anchor:--carousel] [&::scroll-button(*)]:[align-self:anchor-center] [&::scroll-button(right)]:right-[calc(anchor(right)+2rem)] [&::scroll-button(left)]:left-[calc(anchor(left)+2rem)]",
+          // - Content in the buttons
+          "[&::scroll-button(left)]:content-['Previous'] [&::scroll-button(right)]:content-['Next']",
+          // - Style the buttons
+          "[&::scroll-button(*)]:p-4 [&::scroll-button(*)]:border [&::scroll-button(*)]:bg-white [&::scroll-button(*)]:rounded-full",
 
-        // Scroll buttons
-        "[&::scroll-button(left)]:content-[''/'Forrige'] [&::scroll-button(right)]:content-[''/'Neste']",
-        "[&::scroll-button(*)]:p-4 [&::scroll-button(*)]:border [&::scroll-button(*)]:bg-white [&::scroll-button(*)]:rounded-full"
-      )}
-    >
-      {children}
+          // Scroll marker groups (dots)
+          // - Style the group
+          "[scroll-marker-group:after] [&::scroll-marker-group]:mt-4 [&::scroll-marker-group]:flex [&::scroll-marker-group]:justify-center [&::scroll-marker-group]:gap-2",
+          // - Style the markers
+          "[&>*::scroll-marker]:content-[''] [&>*::scroll-marker]:inline-flex [&>*::scroll-marker]:size-6 [&>*::scroll-marker]:bg-white [&>*::scroll-marker]:border [&>*::scroll-marker]:rounded-full",
+          // - Style the active marker
+          "[&>*::scroll-marker:target-current]:bg-amber-500"
+        )}
+      >
+        {children}
+      </div>
     </div>
   );
 }
